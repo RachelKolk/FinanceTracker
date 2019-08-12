53b56790d9ada03182f38e59d2ac2bafdaf6f56e
RachelKolk <rach.m.obrien@gmail.com>
2019-08-12 15:08:45 -0400

    stock lookup form and route/controller

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -10,11 +10,16 @@ class Stock < ApplicationRecord
     begin
         client = IEX::Api::Client.new(publishable_token: 'pk_9ed33d1688b244358bcbfdc5d0d68ac8')
         looked_up_stock = client.quote(ticker_symbol)
-        new(name: looked_up_stock.company_name,
-            ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
+        price = strip_commas(look_up_stock.l)
+        new(name: looked_up_stock.name,
+            ticker: looked_up_stock.symbol, last_price: price)
     rescue Exception => e
         return nil
     end
+  end
+
+  def self.strip_commas
+    number.gsub(",", "")
   end
 
 end
